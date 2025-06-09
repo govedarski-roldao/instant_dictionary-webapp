b2719da55e411e88b7205710fb648d80c44c58a4
Rodolfo Govedarski-Roldão <r_roldao@hotmail.com>
2025-06-09 23:27:45 +0300

    start creation of page

--- definition.py
+++ definition.py
@@ -1,7 +1,19 @@
+import pandas
+
+
 class Definition:
 
     def __init__(self, term):
         self.term = term
 
     def get(self):
-        pass
+        df = pandas.read_csv("data.csv")
+        filter_df = df.loc[df['word'] == self.term]
+        v2 = filter_df["definition"]
+        result = tuple(v2)
+        print(result[0])
+        return result
+
+
+defe = Definition("sun").get()
+print(defe)
