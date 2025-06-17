9ca8f33ebde3220faaa8c1501e6f2998e97b40a3
Rodolfo Govedarski-Roldão <r_roldao@hotmail.com>
2025-06-17 23:40:03 +0300

    create home page

--- webapp/about.py
+++ webapp/about.py
@@ -1,6 +1,23 @@
 import justpy as jp
 
 
-@jp.SetRoute("/about")
-def about_page():
-    pass
+class About:
+    path = "/about"
+
+    def serve(self):
+        wp = jp.QuasarPage()
+        div = jp.Div(a=wp, classes="bg-gray-200 h-screen")
+        jp.Div(a=div, text="This is the about page!", classes="text-4xl m-2")
+        jp.Div(a=div, text="""
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+           Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+            deserunt mollit anim id est laborum.
+        """, classes='text-lg')
+        return wp
+
+
+jp.Route(About.path, About.serve)
+jp.justpy(port=8001)
